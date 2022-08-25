58357ebbb60206ac7638a4fb3c14b677070b546c
Airat Bakiev <bakievaa@mail.ru>
2022-08-25 20:04:57 +0500

    Update User model.

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -38,7 +38,7 @@ class User(AbstractUser):
         ]
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name} ({self.username})'
 
 
 class Subscribe(models.Model):
